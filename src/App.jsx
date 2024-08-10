0db1a1bc65f145f1b3bcf1c400a51c7807487e0d
Eksaron <semencatenkos@gmail.com>
2024-08-10 10:25:38 +0300

    rewrites

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// src/components/App.jsx
+
+import { Routes, Route } from "react-router-dom";
+import Home from "path/to/pages/Home";
+import About from "path/to/pages/About";
+import Products from "path/to/pages/Products";
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
+};
